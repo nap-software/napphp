e8815f20af5913d605fea73e32c44cdca878d012
SZ-Coder <sz-coder@nap.software>
2022-08-29 15:25:46 +0200

    Added __loadAsClass.php to load napphp as an abstract class. Example usage can be found in example-with-class.php.

---
+++ example-with-class.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__."/src/__loadAsClass.php";
+
+# Create random identifier of length 10
+echo napphp::util_randomIdentifier(10)."\n";
---
+++ src/__loadAsClass.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class napphp {
+	static private $_napphp = NULL;
+
+	static public function __callStatic($fn_name, $fn_args) {
+		if (self::$_napphp === NULL) {
+			self::$_napphp = require __DIR__."/__load.php";
+		}
+
+		return self::$_napphp->getInstance()->invoke(
+			$fn_name
+			...$fn_args
+		);
+	}
+}
