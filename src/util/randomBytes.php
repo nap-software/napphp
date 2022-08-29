d7f46fcbbbf913c08eb1e7aee1fb1c3a64ca2111
SZ-Coder <sz-coder@nap.software>
2022-08-29 15:24:37 +0200

    Check return value of openssl_random_pseudo_bytes to prevent a possible infinite loop.

--- src/util/randomBytes.php
+++ src/util/randomBytes.php
@@ -4,6 +4,12 @@ return function($length) {
 	$bytes = [];
 	$bytes_str = openssl_random_pseudo_bytes($length);
 
+	if (strlen($bytes_str) !== $length) {
+		$this->raiseError(
+			"Failed to create random bytes."
+		);
+	}
+
 	for ($i = 0; $i < strlen($bytes_str); ++$i) {
 		array_push(
 			$bytes, ord($bytes_str[$i])
