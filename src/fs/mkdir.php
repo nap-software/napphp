55f011ff33c9858e61f114114e5661996ac9bb7b
SZ-Coder <sz-coder@nap.software>
2022-08-30 22:40:14 +0200

    Changed behavior of fs_mkdir to create sub-directories if they don't exist.

--- src/fs/mkdir.php
+++ src/fs/mkdir.php
@@ -1,7 +1,7 @@
 <?php
 
-return function ($dir) {
-	if (!mkdir($dir)) {
+return function ($dir, $perm = 0777) {
+	if (!mkdir($dir, $perm, true)) {
 		$is_dir = $this->invoke("fs_isDirectory", $dir);
 
 		// Only raise error IF $dir is not a directory after creation
