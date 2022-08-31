6c679f3c5a12424a2addc3dea19bc9afe7d9e139
SZ-Coder <sz-coder@nap.software>
2022-08-31 23:21:09 +0200

    Fix __loadAsClass file.

--- src/__loadAsClass.php
+++ src/__loadAsClass.php
@@ -3,20 +3,30 @@
 abstract class napphp {
 	static private $_napphp = NULL;
 
-	static public function set($key, $value) {
-		return self::int_getInstanceObject()->int_storeSetKey($key, $value);
-	}
-
-	static public function get($key) {
-		return self::int_getInstanceObject()->int_storeGetKey($key);
-	}
-
-	static public function __callStatic($fn_name, $fn_args) {
+	static private function int_getLoadedInstance() {
 		if (self::$_napphp === NULL) {
 			self::$_napphp = require __DIR__."/__load.php";
 		}
 
-		return self::$_napphp->int_getInstanceObject()->invoke(
+		return self::$_napphp->int_getInstanceObject();
+	}
+
+	static public function set($key, $value) {
+		$napphp_instance = self::int_getLoadedInstance();
+
+		return $napphp_instance->int_storeSetKey($key, $value);
+	}
+
+	static public function get($key) {
+		$napphp_instance = self::int_getLoadedInstance();
+
+		return $napphp_instance->int_storeGetKey($key);
+	}
+
+	static public function __callStatic($fn_name, $fn_args) {
+		$napphp_instance = self::int_getLoadedInstance();
+
+		return $napphp_instance->invoke(
 			$fn_name,
 			...$fn_args
 		);
