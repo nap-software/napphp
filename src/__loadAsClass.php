4a4197b6cb115a450aecb78ffeffd5217d2bd346
SZ-Coder <sz-coder@nap.software>
2022-08-31 23:09:08 +0200

    Add ::getInstance() to napphp class.

--- src/__loadAsClass.php
+++ src/__loadAsClass.php
@@ -3,6 +3,14 @@
 abstract class napphp {
 	static private $_napphp = NULL;
 
+	static public function getInstance() {
+		if (self::$_napphp === NULL) {
+			self::$_napphp = require __DIR__."/__load.php";
+		}
+
+		return self::$_napphp->getInstance();
+	}
+
 	static public function __callStatic($fn_name, $fn_args) {
 		if (self::$_napphp === NULL) {
 			self::$_napphp = require __DIR__."/__load.php";
