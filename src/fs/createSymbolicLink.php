af6e2a656b6ba246ad6cd16c903ed0051ae215fc
SZ-Coder <sz-coder@nap.software>
2022-09-01 00:05:49 +0200

    Add comment.

--- src/fs/createSymbolicLink.php
+++ src/fs/createSymbolicLink.php
@@ -2,6 +2,8 @@
 
 return function($dest, $target) {
 	if (!symlink($target, $dest)) {
+		// maybe add is_link() check after creation
+
 		$this->int_raiseError(
 			"Failed to create symbolic link '$target' (existing) -> '$dest'."
 		);
