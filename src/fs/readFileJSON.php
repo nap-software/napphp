5ff5f5fc047de2bf253336ec155a9fd86233fc7b
SZ-Coder <sz-coder@nap.software>
2022-09-01 00:55:45 +0200

    Added $as_array to fs_readFileJSON.

--- src/fs/readFileJSON.php
+++ src/fs/readFileJSON.php
@@ -1,8 +1,8 @@
 <?php
 
-return function($file) {
+return function($file, $as_array = true) {
 	$json = $this->fs_readFileString($file);
-	$data = $this->json_decode($json);
+	$data = $this->json_decode($json, $as_array);
 
 	return $data;
 };
