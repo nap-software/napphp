00ebe733e189b56213911842bd255c51955ca63a
SZ-Coder <sz-coder@nap.software>
2022-09-03 02:07:41 +0200

    Fix arr_join fixed $glue parameter.

--- __tests__/arr/join.php
+++ __tests__/arr/join.php
@@ -4,4 +4,5 @@ testing::case("should work as expected", function() {
 	$array = [1, 2, 3, 4, 5, 6];
 
 	testing::assert(napphp::arr_join($array, "/") === "1/2/3/4/5/6");
+	testing::assert(napphp::arr_join($array, ".") === "1.2.3.4.5.6");
 });
--- src/arr/join.php
+++ src/arr/join.php
@@ -1,5 +1,5 @@
 <?php
 
-return function($array) {
-	return implode("/", $array);
+return function($array, $glue) {
+	return implode($glue, $array);
 };
