4d4d3a25eb4cecdd0ccdbcaded804e1fa7038659
SZ-Coder <sz-coder@nap.software>
2022-09-15 18:20:51 +0200

    Further debugging of failing fs_getFileMode test.

--- __tests__/fs/getFileMode.php
+++ __tests__/fs/getFileMode.php
@@ -5,5 +5,12 @@ testing::case("should work as expected", function() {
 		__DIR__."/test-files/perm-1"
 	);
 	var_dump($mode);
+
+	system(
+		"stat -c '%A %a %h %U %G %s %y %n' ".escapeshellarg(
+			__DIR__."/test-files/perm-1"
+		)
+	);
+
 	testing::assert($mode === 0742);
 });
