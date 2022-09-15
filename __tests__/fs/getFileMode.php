0e7847a21e842ed673b94a0d8bd1770b776e988b
SZ-Coder <sz-coder@nap.software>
2022-09-15 18:28:16 +0200

    Do not execute fs_getFileMode unit test because git does not save permissions bit other than executable.

--- __tests__/fs/getFileMode.php
+++ __tests__/fs/getFileMode.php
@@ -1,16 +1,21 @@
 <?php
 
+/*
+
+Not possible to test in CI environment because git does not retain
+any other permission bits than "executable":
+https://stackoverflow.com/a/11231682/2005038
+
+This is by design. While the git data structure can technically store unix mode bits in its trees,
+it was found early on in git's history that respecting anything beyond a simple executable
+bit ended up being more cumbersome for git's normal use cases (i.e., people storing code
+or other shared files in a repository).
+
 testing::case("should work as expected", function() {
 	$mode = napphp::fs_getFileMode(
 		__DIR__."/test-files/perm-1"
 	);
-	var_dump($mode);
-
-	system(
-		"stat -c '%A %a %h %U %G %s %y %n' ".escapeshellarg(
-			__DIR__."/test-files/perm-1"
-		)
-	);
 
 	testing::assert($mode === 0742);
 });
+*/
