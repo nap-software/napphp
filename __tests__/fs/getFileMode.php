1cbb131d60e688defd58ff7bb21ee67b8875e872
SZ-Coder <sz-coder@nap.software>
2022-09-15 18:18:30 +0200

    Debug: failing fs_getFileMode test.

--- __tests__/fs/getFileMode.php
+++ __tests__/fs/getFileMode.php
@@ -4,6 +4,6 @@ testing::case("should work as expected", function() {
 	$mode = napphp::fs_getFileMode(
 		__DIR__."/test-files/perm-1"
 	);
-
+	var_dump($mode);
 	testing::assert($mode === 0742);
 });
