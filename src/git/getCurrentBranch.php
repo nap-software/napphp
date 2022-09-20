521521edc298ab6a7aff9350ea2b163e138e2f35
SZ-Coder <sz-coder@nap.software>
2022-09-21 00:00:38 +0200

    Remove dependeny of proc_exec.

--- src/git/getCurrentBranch.php
+++ src/git/getCurrentBranch.php
@@ -4,9 +4,13 @@ return function() {
 	if (strlen(getenv("NAPPHP_OVERRIDE_GIT_BRANCH"))) {
 		$git_branch = getenv("NAPPHP_OVERRIDE_GIT_BRANCH");
 	} else {
-		$git_branch = $this->proc_exec(
-			"git rev-parse --abbrev-ref HEAD"
-		)[0];
+		exec("git rev-parse --abbrev-ref HEAD", $output, $exit_code);
+
+		if ($exit_code !== 0) {
+			$this->int_raiseError("Failed to execute 'git rev-parse'.");
+		}
+
+		$git_branch = $output[0];
 	}
 
 	return trim($git_branch);
--- src/git/getHEADSha1Hash.php
+++ src/git/getHEADSha1Hash.php
@@ -1,7 +1,11 @@
 <?php
 
 return function() {
-	return trim(
-		$this->proc_exec("git rev-parse HEAD")[0]
-	);
+	exec("git rev-parse HEAD", $output, $exit_code);
+
+	if ($exit_code !== 0) {
+		$this->int_raiseError("Failed to execute 'git rev-parse HEAD'.");
+	}
+
+	return trim($output[0]);
 };
